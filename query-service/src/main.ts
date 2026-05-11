aba952f9e15989489d45d0475ac951109ee54fad
CBG003 <cherifabenghorbel@gmail.com>
2026-05-11 17:32:00 +0100

    made the querry work

--- query-service/src/main.ts
+++ query-service/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json());
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
